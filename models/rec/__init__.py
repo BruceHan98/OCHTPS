cf0b33f0bbc9a0b78226b61db6bdc8c8917eea37
BruceHan98 <brucehan1998@outlook.com>
2021-09-25 18:35:51 +0800

    OCHTPS

---
+++ models/rec/__init__.py
@@ -0,0 +1,7 @@
+from .rec_layer_new_with_tcn_big import DenseNet as DenseNet_with_TCN_big
+from .rec_layer_new_with_tcn_big import DenseNet as DenseNet_with_TCN_big
+
+from .rec_layer_new_with_tcn_big import DenseNet as DenseNet_with_TCN_big
+
+from .rec_layer_new_with_tcn_big import DenseNet as DenseNet_with_TCN_big
+
